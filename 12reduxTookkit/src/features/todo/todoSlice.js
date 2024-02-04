6e2e2db12661f53ccdc523ac09e760f98ea43836
Souvik Maity <87945067+souvikmaity100@users.noreply.github.com>
2024-02-05 01:29:05 +0530

    add update mode

--- 12reduxTookkit/src/features/todo/todoSlice.js
+++ 12reduxTookkit/src/features/todo/todoSlice.js
@@ -1,29 +1,43 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-    todos: [{id: 1, text: 'hello'}]
-}
-
-
+  todos: [{ id: 1, text: "hello" }],
+  editMode: {isEditable: false, editId: 0, editValue: ''}
+};
 
 export const todoSlice = createSlice({
-    name: 'todo',
-    initialState,
-    reducers: {
-        addTodo: (state, action) => {
-            const todo = {
-                id: nanoid(),
-                text: action.payload
-            }
-            state.todos.push(todo)
-        },
-        removeTodo: (state, action) => {
-            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
-        }
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    updateTodo: (state, action) => {
+      const { id, text } = action.payload;
+      state.todos = state.todos.map((todo) => {
+        let text2 = (todo.id == id) ? text : todo.text
+        let id2 = (todo.id == id) ? id : todo.id
+        return { id: id2, text: text2 }
+      });
+    },
+    updateEditMode: (state, action) => {
+        const {edit, editId} = action.payload
+        state.editMode.isEditable = edit
+        state.editMode.editId = editId
+    },
+    updateInputVal: (state, action) => {
+        state.editMode.editValue = action.payload
     }
-})
+  },
+});
 
+export const { addTodo, removeTodo, updateTodo, updateEditMode, updateInputVal } = todoSlice.actions;
 
-export const {addTodo, removeTodo} = todoSlice.actions
-
-export default todoSlice.reducer
+export default todoSlice.reducer;
